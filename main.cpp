b6444e3fcf735a466951d33ae3f103f5d5700e9a
Mauro M <m4uro.m@gmail.com>
2017-04-14 16:42:14 +1200

    Replace QQmlApplicationEngine with QQuickView

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QQuickView>
+#include <QUrl>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    QQuickView view;
+
+    view.setSource(QUrl("qrc:/main.qml"));
+    view.setWidth(600);
+    view.setHeight(400);
+    view.show();
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,11 +1,7 @@
 import QtQuick 2.5
-import QtQuick.Window 2.2
 
-Window {
-    visible: true
-    width: 640
-    height: 480
-    title: qsTr("Hello World")
+Rectangle {
+    anchors.fill: parent
 
     Text {
         anchors.centerIn: parent
