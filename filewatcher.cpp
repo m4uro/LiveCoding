b1c5d9396b4845f7f1af942b60f51008033c4dbc
Mauro M <m4uro.m@gmail.com>
2017-04-16 10:50:03 +1200

    Reload main qml file on change detected. Add qml filters for watcher

--- filewatcher.cpp
+++ filewatcher.cpp
@@ -1,6 +1,5 @@
 #include "filewatcher.h"
 #include <QDebug>
-#include <iostream>
 
 /**
  * @brief FileWatcher::FileWatcher
@@ -19,44 +18,23 @@ FileWatcher::FileWatcher(std::function<void()> callback)
     mTimer.setInterval(100);
     mTimer.setSingleShot(true);
 
+    // configure directory filters
+    mDir.setFilter(QDir::Files | QDir::NoSymLinks);
+    mDir.setNameFilters(QStringList() << "*.qml");
+
+    // connect timer to callback function
     QObject::connect(&mTimer, &QTimer::timeout, mCallback);
-}
-
-namespace {
-    void ListDirectory(const QString &path)
-    {
-       QDir dir(path);
-       dir.setFilter(QDir::Files | QDir::NoSymLinks);
-
-       QFileInfoList list = dir.entryInfoList();
-       std::cout << "     Bytes Filename" << std::endl;
-       for (int i = 0; i < list.size(); ++i) {
-           QFileInfo fileInfo = list.at(i);
-           std::cout << qPrintable(QString("%1 %2").arg(fileInfo.size(), 10)
-                                                   .arg(fileInfo.fileName()));
-           std::cout << std::endl;
-       }
-    }
 }
 
 void FileWatcher::addPaths()
 {
-    QStringList entries = mDir.entryList(QDir::Files | QDir::NoSymLinks);
+    QStringList entries = mDir.entryList();
     for (int i=0; i<entries.size(); i++)
     {
         entries[i].prepend(mDir.absolutePath() + "/");
     }
 
-    qDebug() << "Adding paths: " << entries;
-    QStringList not_watched = mWatcher.addPaths(entries);
-
-    if (!not_watched.empty())
-    {
-        qWarning() << "Warning - These files could not be watched: " << not_watched;
-    }
-
-    qDebug() << "files being watched: " << mWatcher.files();
-
+    mWatcher.addPaths(entries);
     mTimer.start();
 }
 
@@ -68,32 +46,23 @@ void FileWatcher::addPaths()
  */
 void FileWatcher::setDirectory(const QString &path)
 {
-    qDebug() << "setting directory to: " << path;
-
     if (!mWatcher.addPath(path))
     {
         qWarning() << "Could not add path: " << path;
     }
     else
     {
-        ListDirectory(path);
-        mDir.setCurrent(path);
-        // add paths for each file to the watcher
+        mDir.setPath(path);
         addPaths();
     }
 }
 
 void FileWatcher::directoryChanged(const QString &path)
 {
-    qDebug() << "FileWatcher::directoryChanged: " << path;
-
-    // add paths for each file to the watcher
     addPaths();
 }
 
 void FileWatcher::fileChanged(const QString &path)
 {
-    qDebug() << "FileWatcher::fileChanged: " << path;
-    qDebug() << "files being watched: " << mWatcher.files();
     mTimer.start();
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <QGuiApplication>
 #include <QQuickView>
 #include <QUrl>
+#include <QQmlEngine>
 
-#include <QDebug>
 #include "filewatcher.h"
 
 int main(int argc, char *argv[])
@@ -11,13 +11,20 @@ int main(int argc, char *argv[])
 
     QQuickView view;
 
-    view.setSource(QUrl("qrc:/main.qml"));
+    const QDir DIRECTORY("D:/Dev/qt projects/LiveCoding");
+    const QUrl SOURCE_URL = QUrl::fromLocalFile(DIRECTORY.filePath("main.qml"));
+
+    view.setSource(SOURCE_URL);
     view.setWidth(600);
     view.setHeight(400);
     view.show();
 
-    FileWatcher watcher([](){ qDebug() << "executing callback function";});
-    watcher.setDirectory("D:/Dev/qt projects/test");
+    FileWatcher watcher([&view, SOURCE_URL](){
+        view.engine()->clearComponentCache();
+        view.setSource(SOURCE_URL);
+    });
+
+    watcher.setDirectory(DIRECTORY.absolutePath());
 
     return app.exec();
 }
